e85d84575eaac7e22e6e37e462db8bec404bfec6
muhammed-radin <rasiyakaratt@gmail.com>
2022-01-09 20:37:40 +0530

    top bar created

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log('Hello World!');
+
+//let date = new Date()
+//let topBarTime = document.getElementById('time')
+//let currentDate = date.getFullYear()+" "+date.getMonth()+1+" "+date.getHours()+" "+date.getSeconds()
+setInterval(
+  function() {
+    let date = new Date()
+    let topBarTime = document.getElementById('time')
+    let currentDate = date.getFullYear() + " " + date.getMonth() + 1 + " " + date.getHours() + " " + date.getMinutes() + " " + date.getSeconds()
+
+    topBarTime.innerHTML = currentDate
+    // console.log(date)
+  }, 100)
