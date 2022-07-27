3336f77aa6ea6b1a2b843fce7c6e3f428c76af50
Muhammed Rahif <rahifpalliyalil@gmail.com>
2022-07-27 20:35:09 +0530

    data updated

--- data.js
+++ data.js
@@ -2,6 +2,9 @@
   script: [{
     apps: [{
       appName: "Alex's Story",
+      id: 368213,
+      date: '27/7/2022',
+      no: 1,
       devName: "radin",
       version: '1.0.0',
       tags: [
@@ -19,7 +22,8 @@
           'https://img.icons8.com/fluency/48/000000/image.png',
           'https://img.icons8.com/fluency/48/000000/image.png',
           'https://img.icons8.com/fluency/48/000000/image.png'
-          ]
+          ],
+          thumb: 'https://img.icons8.com/fluency/608/000000/image.png',
       },
     }],
   }]
