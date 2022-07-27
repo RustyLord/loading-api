8c9acf94502df55fb9803bd51cc0ac140537ac66
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2022-07-27 19:00:13 +0530

    api updated

--- data.js
+++ data.js
@@ -1,3 +1,26 @@
 {
-  name: "api requested"
+  script: [{
+    apps: [{
+      appName: "Alex's Story",
+      devName: "radin",
+      version: '1.0.0',
+      tags: [
+        'adventure',
+        'storymode'
+        ],
+      source: [{
+        url: 'https://img.icons8.com/fluency/48/000000/image.png',
+        type: 'png'
+      }],
+      imgUrl: {
+        icon: 'https://img.icons8.com/fluency/48/000000/image.png',
+        screenshots: [
+          'https://img.icons8.com/fluency/48/000000/image.png',
+          'https://img.icons8.com/fluency/48/000000/image.png',
+          'https://img.icons8.com/fluency/48/000000/image.png',
+          'https://img.icons8.com/fluency/48/000000/image.png'
+          ]
+      },
+    }],
+  }]
 }
